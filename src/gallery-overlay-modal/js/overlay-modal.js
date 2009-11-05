2f90cba912f54e4246c4ab6b299bb0c8e5b2992e
Eric Ferraiuolo <eferraiuolo@gmail.com>
2009-11-05 00:36:24 -0500

    no need to double append the mask node.

--- src/gallery-overlay-modal/js/overlay-modal.js
+++ src/gallery-overlay-modal/js/overlay-modal.js
@@ -88,8 +88,6 @@
 		
 		renderUI : function () {
 			
-			var boundingBox = this.get(HOST).get(BOUNDING_BOX);
-			
 			this._maskNode = Y.Node.create('<div></div>');
 			this._maskNode.addClass(OverlayModal.CLASSES.mask);
 			this._maskNode.setStyles({
@@ -101,8 +99,7 @@
 				zIndex		: '-1'
 			});
 			
-			boundingBox.append(this._maskNode);
-			boundingBox.addClass(OverlayModal.CLASSES.modal);
+			this.get(HOST).get(BOUNDING_BOX).addClass(OverlayModal.CLASSES.modal);
 		},
 		
 		bindUI : function () {
